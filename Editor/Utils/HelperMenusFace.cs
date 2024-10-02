1688590b83e085c5470959b359140041ca0f9ad3
Andrew Kim <andkim@meta.com>
2024-10-02 11:31:08 -0700

    feat(Editor): Remove unused duplicate face quick action
    Summary: In the available quick actions for Face Tracking, there is currently an option to setup the face with duplicates. This is unused in the samples, and isn't recommended in the majority of cases.
    
    Reviewed By: sohailshafiiWk
    
    Differential Revision: D63770386
    
    fbshipit-source-id: 08cefd53bb6c62722a4c111b03bce6618d8d191f

--- Editor/Utils/HelperMenusFace.cs
+++ Editor/Utils/HelperMenusFace.cs
@@ -15,20 +15,9 @@ namespace Oculus.Movement.Utils
             "Correctives Face";
         private const string _ARKIT_FACE_MENU =
             "ARKit Face";
-        private const string _NO_DUPLICATES_SUFFIX =
-            " (duplicate mapping off)";
 
         [MenuItem(AddComponentsHelper._MOVEMENT_SAMPLES_MENU + _MOVEMENT_SAMPLES_FT_MENU +
             _CORRECTIVES_FACE_MENU)]
-        private static void SetupCharacterForCorrectivesFace()
-        {
-            var activeGameObject = Selection.activeGameObject;
-
-            AddComponentsHelper.SetUpCharacterForCorrectivesFace(activeGameObject, true);
-        }
-
-        [MenuItem(AddComponentsHelper._MOVEMENT_SAMPLES_MENU + _MOVEMENT_SAMPLES_FT_MENU +
-            _CORRECTIVES_FACE_MENU + _NO_DUPLICATES_SUFFIX)]
         private static void SetupCharacterForCorrectivesFaceNoDuplicates()
         {
             var activeGameObject = Selection.activeGameObject;
@@ -37,15 +26,6 @@ namespace Oculus.Movement.Utils
         }
 
         [MenuItem(AddComponentsHelper._MOVEMENT_SAMPLES_MENU + _MOVEMENT_SAMPLES_FT_MENU + _ARKIT_FACE_MENU)]
-        private static void SetupCharacterForARKitFace()
-        {
-            var activeGameObject = Selection.activeGameObject;
-
-            AddComponentsHelper.SetUpCharacterForARKitFace(activeGameObject, true);
-        }
-
-        [MenuItem(AddComponentsHelper._MOVEMENT_SAMPLES_MENU + _MOVEMENT_SAMPLES_FT_MENU + _ARKIT_FACE_MENU
-            + _NO_DUPLICATES_SUFFIX)]
         private static void SetupCharacterForARKitFaceNoDuplicates()
         {
             var activeGameObject = Selection.activeGameObject;
